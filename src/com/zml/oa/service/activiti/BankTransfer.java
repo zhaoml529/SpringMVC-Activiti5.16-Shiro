0a9a8a11f29fd48748228ef16d360b7735e25b99
zml <zhaoml529@163.com>
2014-12-11 11:16:01 +0800

    演示ServiceTask中 activiti:class 属性的使用

---
+++ src/com/zml/oa/service/activiti/BankTransfer.java
@@ -0,0 +1,37 @@
+package com.zml.oa.service.activiti;
+
+import java.math.BigDecimal;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.BpmnError;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zml.oa.entity.ExpenseAccount;
+
+/**
+ * 此方法演示使用ServiceTask的activiti:class 属性执行Java类
+ * 该java类必须是JavaDelegate或者ActivitiBehavior的实现类
+ * @author ZML
+ *
+ */
+public class BankTransfer implements JavaDelegate {
+
+    @Autowired
+    protected RuntimeService runtimeService;
+    
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		ExpenseAccount expense = (ExpenseAccount)this.runtimeService.getVariable(execution.getProcessInstanceId(), "entity");
+		if (expense.getMoney().compareTo(new BigDecimal(1000)) == 1 ) {
+			System.out.println("银行转账失败");
+			throw new BpmnError("to much");
+		} else {
+			//具体业务
+			System.out.println("银行转帐成功");
+		}
+	
+	}
+
+}
