3e52bba8ee5ea8fb16af99104eefa0d3ca1e1945
zhaoml529 <zhaoml529@163.com>
2015-03-09 23:57:57 +0800

    public static String DB_NAME = "mysql";

--- src/com/zml/oa/util/Constants.java
+++ src/com/zml/oa/util/Constants.java
@@ -1,11 +1,15 @@
 package com.zml.oa.util;
 
+
 /**
  * <p>User: Zhang Kaitao
  * <p>Date: 14-2-15
  * <p>Version: 1.0
  */
 public class Constants {
+	
+	/***************** system *****************/
+	public static String DB_NAME = "mysql";
 	
 	/***************** session key *****************/
     public static final String CURRENT_USER = "user";
