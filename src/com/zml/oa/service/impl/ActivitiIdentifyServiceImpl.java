82815be8393118579fcccfcef6e8b9648337c79b
zhaoml529 <zhaoml529@163.com>
2015-03-09 23:56:58 +0800

    改用jdbc模版执行sql

--- src/com/zml/oa/service/impl/ActivitiIdentifyServiceImpl.java
+++ src/com/zml/oa/service/impl/ActivitiIdentifyServiceImpl.java
@@ -1,31 +1,39 @@
 package com.zml.oa.service.impl;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.zml.oa.dao.ActivitiIdentifyCommonDao;
+import com.zml.oa.dao.IJdbcDao;
 import com.zml.oa.service.IActivitiBaseService;
 
 @Service
 public class ActivitiIdentifyServiceImpl implements IActivitiBaseService {
 
+	private static final Logger logger = Logger.getLogger(ActivitiIdentifyServiceImpl.class);
+
 	@Autowired
-    protected ActivitiIdentifyCommonDao activitiIdentifyCommonDao;
+	protected IJdbcDao jdbcDao;
 	
 	@Override
 	public void deleteAllUser() {
-		this.activitiIdentifyCommonDao.deleteAllUser();
-
+		String sql = "delete from ACT_ID_USER";
+		this.jdbcDao.delete(sql, null);
+		logger.debug("deleted from activiti user.");
 	}
 
 	@Override
 	public void deleteAllRole() {
-		this.activitiIdentifyCommonDao.deleteAllRole();
+		String sql = "delete from ACT_ID_GROUP";
+		this.jdbcDao.delete(sql, null);
+		logger.debug("deleted from activiti group.");
 	}
 
 	@Override
 	public void deleteAllMemerShip() {
-		this.activitiIdentifyCommonDao.deleteAllMemerShip();
+		String sql = "delete from ACT_ID_MEMBERSHIP";
+		this.jdbcDao.delete(sql, null);
+		logger.debug("deleted from activiti membership.");
 	}
 
 }
